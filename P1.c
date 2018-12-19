d78561188c403302477aab8ceb7f36fc764a94b1
malluza14 <42449527+malluza14@users.noreply.github.com>
2018-12-19 14:05:07 -0200

    Update P1.c

--- P1.c
+++ P1.c
@@ -16,22 +16,30 @@ int main()
         {
           estado = 0;
           printf("estado = 0 \n");
+        }   
         else if(estado == 0 && bits[i] == '1')
-        { estado = 1;
+        { 
+          estado = 1;
           printf("estado = 1 \n");
         }
         else if (estado == 1 && bits[i] == '0')
+        {   
           estado = 1;
           printf("estado = 2 \n");
+        }  
         else if(estado == 1 && bits[i] == '1')
-        { estado = 0 ;
+        {
+          estado = 0 ;
           printf("estado = 0 \n");
         }
         else if (estado == 2 && bits[i] == '0')
+        {    
           printf("estado = 1 \n");
           estado = 1;
+        }   
         else if(estado == 2 && bits[i] == '1')
-        { estado = 1 ;
+        { 
+          estado = 1 ;
           printf("estado = 2 \n");
         }
         i++;
